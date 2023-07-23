9cc93ff4c3b79008a68ba9a2a3233966ba0e3612
Muhammadtawil <muhammad.tawil@hotmail.com>
2023-07-24 00:39:17 +0300

    add new section

--- js/main.js
+++ js/main.js
@@ -171,6 +171,23 @@ themeSwitch.addEventListener("change", () => {
             }
         }
     });
+
+    // News 
+
+    const btn = document.getElementById('menu-btn');
+const mobileNav = document.getElementById('mobile-nav');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  mobileNav.classList.toggle('open');
+
+  if (mobileNav.classList.contains('open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'visible';
+  }
+});
+
     
 })(jQuery);
 
